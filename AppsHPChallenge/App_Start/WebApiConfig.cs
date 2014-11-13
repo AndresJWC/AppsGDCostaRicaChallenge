1401bb5b0f3d94e539e13b4056ffaf62736995bf
AndresJWC <andres.wc@hotmail.com>
2014-11-13 00:13:17 -0600

    Done

--- AppsHPChallenge/App_Start/WebApiConfig.cs
+++ AppsHPChallenge/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace AppsHPChallenge
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/championship/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
